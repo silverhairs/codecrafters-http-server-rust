f7e629780d7d81ba7503d3bc460159ff42075657
Boris Kayi <hello@silverhairs.engineer>
2023-12-18 01:15:48 +0100

    feat: 404 response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
+
+use regex::Regex;
 
 const ADDRESS: &str = "127.0.0.1";
 const PORT: i32 = 4221;
@@ -14,8 +19,28 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("Accepted connection");
+                let mut res = String::new();
+                let mut req = String::new();
+                match _stream.read_to_string(&mut req) {
+                    Ok(_) => {
+                        let pattern = Regex::new(r"^\/[\w\-\/]*$").unwrap();
+                        match req.split("\r\n").find(|s| pattern.is_match(s)) {
+                            Some(path) => {
+                                if path == "/" {
+                                    res = "HTTP/1.1 200 OK\r\n\r\n".to_string();
+                                }
+                            }
+                            None => {
+                                res = "HTTP/1.1 404 Not Found\r\n\r\n".to_string();
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        println!("Failed to read request {}", e);
+                    }
+                };
                 _stream
-                    .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
+                    .write_all(res.as_bytes())
                     .expect("Failed to respond to client");
             }
             Err(e) => {
